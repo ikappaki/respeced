33f02a91f25f15b549666a95a6cdbdf8256c55ba
ikappaki <ikappaki@users.noreply.github.com>
2022-10-27 19:26:46 +0100

    latest

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -4,28 +4,34 @@
             [babashka.process :as p]))
 
 (defn clj-test
-  [alias-clojure-version]
-  (println "=== Running clj-tests" alias-clojure-version)
-  (let [alias (format "-A:test%s:clj-tests" alias-clojure-version)]
+  "Runs the clojure tests using the deps DEPS-CLJ-VER-ALIAS (presumambly
+  an alias that sets the clojure version to use as dep)."
+  [deps-clj-ver-alias]
+  (println "=== Running clj-tests" deps-clj-ver-alias)
+  (let [alias (format "-A:test%s:clj-tests" deps-clj-ver-alias)]
+    (fs/delete-tree "out")
     (-> (deps/clojure [alias])
         p/check)))
 
 (defn cljs-test
-  [alias-clojure-version]
-  (let [alias (format "-A:test%s:compile-node-tests" alias-clojure-version)]
-    (println "\n=== Compiling node tests" alias-clojure-version)
-    (println :alias alias)
+  "Runs the clojure tests using the deps DEPS-CLJ-VER-ALIAS (presumambly
+  an alias that sets the clojurescript version to use as dep)."
+  [deps-clj-ver-alias]
+  (let [alias (format "-A:test%s:compile-node-tests" deps-clj-ver-alias)]
+    (println "\n=== Compiling node tests" deps-clj-ver-alias)
     (-> (deps/clojure [alias])
         p/check)
     (println "\n=== Running node tests")
     (p/shell "node out/main.js")))
 
 (defn plk-test
-  [alias-clojure-version]
-  (println "\n=== Running plk-tests" alias-clojure-version)
+  "Runs the clojure tests using the deps DEPS-CLJ-VER-ALIAS (presumambly
+  an alias that sets the clojurescript version to use as dep)."
+  [deps-clj-ver-alias]
+  (println "\n=== Running plk-tests" deps-clj-ver-alias)
   (if (fs/windows?)
-    (println "\n=== [SKIPPING] plk is not available on MS-Windows!")
+    (println "\n=== [SKIPPING] The Planck REPL is not available on MS-Windows.")
 
-    (let [alias (format "-A:test%s:plk-tests" alias-clojure-version)]
+    (let [alias (format "-A:test%s:plk-tests" deps-clj-ver-alias)]
       (-> (deps/clojure [alias])
           p/check))))
